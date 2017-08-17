aa3a32b3dd77147a423926d8869239d4084fbd7a
Eric Vicenti <ericvicenti@gmail.com>
2017-08-17 07:25:35 -0700

    Progress on pinch to zoom view

--- App.js
+++ App.js
@@ -4,8 +4,11 @@ import {
   Text,
   View,
   Image,
+  Animated,
   ScrollView,
   TouchableOpacity,
+  PanResponder,
+  Dimensions,
   TouchableWithoutFeedback
 } from "react-native";
 import PhotoViewer from "./PhotoViewer";
@@ -60,9 +63,9 @@ const EXTRA_PHOTOS = [
 ];
 const EXTRA_PHOTO = {
   key: "extra-3",
-  width: 200,
-  height: 200,
-  source: { uri: "https://placebear.com/200/204", cache: "force-cache" }
+  width: 600,
+  height: 600,
+  source: { uri: "https://placebear.com/600/600", cache: "force-cache" }
 };
 
 const FEED_ITEMS = [
@@ -130,7 +133,7 @@ const Item = ({ item, mainPhotos, onPhotoOpen }) =>
     </Text>
   </View>;
 
-export default class App extends React.Component {
+class PApp extends React.Component {
   render() {
     return (
       <PhotoViewer
@@ -151,6 +154,141 @@ export default class App extends React.Component {
     );
   }
 }
+
+class PinchToZoom extends React.Component {
+  _screenAspectRatio = this.props.width / this.props.height;
+  _photoAspectRatio = this.props.photo.width / this.props.photo.height;
+  _isWidePhoto = this._screenAspectRatio < this._photoAspectRatio;
+
+  // current photo state
+  _accumScale = this._isWidePhoto
+    ? this.props.width / this.props.photo.width
+    : this.props.height / this.props.photo.height;
+  _accumX = (this.props.width - this.props.photo.width) / 2;
+  _accumY = (this.props.height - this.props.photo.height) / 2;
+
+  // photo animation values:
+  _xOffset = new Animated.Value(this._accumX);
+  _yOffset = new Animated.Value(this._accumY);
+  _scale = new Animated.Value(this._accumScale);
+
+  // gesture state:
+  _pinchStartDistance = null;
+  _currentPinchScale = 1;
+  _currentGestureX = 0;
+  _currentGestureY = 0;
+
+  _animateTo = (animValue, destValue) => {
+    Animated.spring(animValue, {
+      toValue: destValue,
+      useNativeDriver: true,
+      friction: 4,
+      tension: 40,
+      duration: 200
+    }).start();
+    // animValue.setValue(destValue);
+  };
+
+  _setResponderMove = (e, gestureState) => {
+    this._currentGestureX = gestureState.dx;
+    this._currentGestureY = gestureState.dy;
+    this._currentPinchScale = 1;
+    const { touches } = e.nativeEvent;
+
+    if (touches.length === 2) {
+      const deltaX = touches[0].pageX - touches[1].pageX;
+      const deltaY = touches[0].pageY - touches[1].pageY;
+      const delta = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+      if (!this._pinchStartDistance) {
+        this._pinchStartDistance = delta;
+      } else {
+        const pinchVal = delta / this._pinchStartDistance;
+        this._currentPinchScale = pinchVal;
+      }
+    } else {
+      this._pinchStartDistance = null;
+    }
+    if (this._moveTimeout) {
+      return;
+    }
+    this._moveTimeout = setTimeout(() => {
+      const destXOffset =
+        this._currentGestureX / this._accumScale + this._accumX;
+      const destYOffset =
+        this._currentGestureY / this._accumScale + this._accumY;
+      const destScale = this._currentPinchScale * this._accumScale;
+      this._animateTo(this._xOffset, destXOffset);
+      this._animateTo(this._yOffset, destYOffset);
+      this._animateTo(this._scale, destScale);
+      this._moveTimeout = null;
+    }, 80);
+  };
+
+  _panResponder = PanResponder.create({
+    onMoveShouldSetPanResponder: (e, gestureState) => {
+      return true;
+    },
+    onPanResponderGrant: (e, gestureState) => {
+      // console.log("granted!");
+    },
+    onPanResponderMove: (e, gestureState) => {
+      this._setResponderMove(e, gestureState);
+    },
+    onPanResponderRelease: (e, gestureState) => {
+      clearTimeout(this._moveTimeout);
+      this._moveTimeout = null;
+      this._accumX = this._accumX + this._currentGestureX;
+      this._accumY = this._accumY + this._currentGestureY;
+      this._accumScale = this._accumScale * this._currentPinchScale;
+      console.log(this._accumScale, this._currentPinchScale);
+
+      // this._offset = (this.props.width - this.props.photo.width) / 2;
+
+      const a = this._accumX + this.props.photo.width / 2;
+
+      this._currentGestureX = 0;
+      this._currentGestureY = 0;
+      this._currentPinchScale = 1;
+    }
+  });
+  render() {
+    console.log({
+      photo: this._photoAspectRatio,
+      screen: this._screenAspectRatio
+    });
+    const { photo } = this.props;
+    return (
+      <View
+        style={{ flex: 1, backgroundColor: "black" }}
+        {...this._panResponder.panHandlers}
+      >
+        <Animated.View
+          style={{
+            transform: [
+              { scale: this._scale },
+              { translateX: this._xOffset },
+              { translateY: this._yOffset }
+            ]
+          }}
+        >
+          <Image
+            source={photo.source}
+            style={{ width: photo.width, height: photo.height }}
+          />
+        </Animated.View>
+      </View>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    const { width, height } = Dimensions.get("window");
+    return <PinchToZoom photo={EXTRA_PHOTO} width={width} height={height} />;
+  }
+}
+
+module.exports = App;
 
 const styles = StyleSheet.create({
   container: {
